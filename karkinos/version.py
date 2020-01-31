76c12454ace26df1a86d7fe70103077a04c85f5a
0xb0bb <49746578+0xb0bb@users.noreply.github.com>
2020-02-01 00:17:55 +0100

    Create version.py
    Attempting to test update

---
+++ karkinos/version.py
@@ -0,0 +1,10 @@
+##
+##  b0bb - 31/01/2020 - Karkinos
+##
+##  https://twitter.com/0xb0bb
+##  https://github.com/0xb0bb/karkinos
+##
+
+MAJOR_VERSION    = 0
+MINOR_VERSION    = 1
+KARKINOS_VERSION = 'Karkinos v%d.%d' % (MAJOR_VERSION, MINOR_VERSION)
